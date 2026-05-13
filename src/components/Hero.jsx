cfd3533f90329eec03d38f7b90f5b8936fc59152
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2026-05-13 23:38:25 +0600

    added hero section in this applications

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+const Hero = () => {
+  return (
+    <div>
+      <div className="hero bg-base-200 py-10">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Welcome to our online store! We offer a wide range of products at
+              unbeatable prices. Shop with us and experience the best deals on
+              the market.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,21 @@
 import { Suspense } from "react";
+import Hero from "../components/Hero";
 import LatestProducts from "../components/LatestProducts";
 
 const Home = () => {
   const latestProductsPromise = fetch(
-    "http://localhost:3000/latest-products"
+    "http://localhost:3000/latest-products",
   ).then((res) => res.json());
   return (
-    <div className="text-secondary">
-      <Suspense fallback={"/loading..."}>
-        <LatestProducts
-          latestProductsPromise={latestProductsPromise}
-        ></LatestProducts>
-      </Suspense>
+    <div>
+      <Hero />
+      <div className="text-secondary">
+        <Suspense fallback={"/loading..."}>
+          <LatestProducts
+            latestProductsPromise={latestProductsPromise}
+          ></LatestProducts>
+        </Suspense>
+      </div>
     </div>
   );
 };
